0e6043c192f0180801eb0a0d67c4c95b270927e6
murillosm <murillosm@hotmail.com>
2022-04-23 23:14:52 -0300

    commit test bootstrap final

--- src/main/java/com/springBootProjetoInicial/springBootProject/service/CodeblogService.java
+++ src/main/java/com/springBootProjetoInicial/springBootProject/service/CodeblogService.java
@@ -4,7 +4,7 @@ import com.springBootProjetoInicial.springBootProject.model.Post;
 
 import java.util.List;
 
-public interface CodeBlogService {
+public interface CodeblogService {
     List<Post> findAll();
     Post findById(long id);
     Post save(Post post);
--- src/main/java/com/springBootProjetoInicial/springBootProject/service/serviceImpl/CodeblogServiceImpl.java
+++ src/main/java/com/springBootProjetoInicial/springBootProject/service/serviceImpl/CodeblogServiceImpl.java
@@ -2,14 +2,14 @@ package com.springBootProjetoInicial.springBootProject.service.serviceImpl;
 
 import com.springBootProjetoInicial.springBootProject.model.Post;
 import com.springBootProjetoInicial.springBootProject.repository.CodeblogRepository;
-import com.springBootProjetoInicial.springBootProject.service.CodeBlogService;
+import com.springBootProjetoInicial.springBootProject.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CodeBlogServiceImpl implements CodeBlogService {
+public class CodeblogServiceImpl implements CodeblogService {
 
     @Autowired
     CodeblogRepository codeblogRepository;
